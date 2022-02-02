b0b32b6138238839657b23a448b7e64246c31370
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-02-02 10:14:44 +0200

    al emlak report

---
+++ amcl_al_emlak/wizard/abstract/nv_abstract_report.py
@@ -0,0 +1,103 @@
+from odoo import fields, models, api
+import logging
+_logger = logging.getLogger(__name__)
+
+
+# تقرير اقرار عادي
+class OrdinaryConfessionReport(models.AbstractModel):
+    _name = 'report.amcl_al_emlak.report_sale_order_ordinary_confession_view'
+    _description = 'تقرير اقرار عادي'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        doc_id = data['id']
+        model = data['model']
+        doc = self.env[data['model']].browse(doc_id)
+        # data['info'] = doc.get_lines()
+        docargs = {
+            'doc_ids': [doc_id],
+            'doc_model': model,
+            'data': data,
+            'docs': [doc],
+        }
+        return docargs
+
+
+# نموذج استلام
+class ReceiptFormReport(models.AbstractModel):
+    _name = 'report.amcl_al_emlak.report_sale_order_receipt_form_view'
+    _description = 'نموذج استلام'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        doc_id = data['id']
+        model = data['model']
+        doc = self.env[data['model']].browse(doc_id)
+        # data['info'] = doc.get_lines()
+        docargs = {
+            'doc_ids': [doc_id],
+            'doc_model': model,
+            'data': data,
+            'docs': [doc],
+        }
+        return docargs
+
+
+# نموذج رقم 4
+class FormNb4CommitmentReport(models.AbstractModel):
+    _name = 'report.amcl_al_emlak.form_nb_4_commitment_of_individuals'
+    _description = 'نموذج رقم 4'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        doc_id = data['id']
+        model = data['model']
+        doc = self.env[data['model']].browse(doc_id)
+        # data['info'] = doc.get_lines()
+        docargs = {
+            'doc_ids': [doc_id],
+            'doc_model': model,
+            'data': data,
+            'docs': [doc],
+        }
+        return docargs
+
+
+# نموذج إقرار المستخدم
+class FormNb4CommitmentReport(models.AbstractModel):
+    _name = 'report.amcl_al_emlak.report_user_acknowledgment_form_view'
+    _description = 'نموذج إقرار المستخدم'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        doc_id = data['id']
+        model = data['model']
+        doc = self.env[data['model']].browse(doc_id)
+        # data['info'] = doc.get_lines()
+        docargs = {
+            'doc_ids': [doc_id],
+            'doc_model': model,
+            'data': data,
+            'docs': [doc],
+        }
+        return docargs
+
+
+#  سند قبض
+class CatchReceiptReport(models.AbstractModel):
+    _name = 'report.amcl_al_emlak.report_catch_receipt_report_view'
+    _description = 'نموذج إقرار المستخدم'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        doc_id = data['id']
+        model = data['model']
+        doc = self.env[data['model']].browse(doc_id)
+        # data['info'] = doc.get_lines()
+        docargs = {
+            'doc_ids': [doc_id],
+            'doc_model': model,
+            'data': data,
+            'docs': [doc],
+        }
+        return docargs
---
+++ amcl_al_emlak/wizard/models/amcl_report_wizard.py
@@ -0,0 +1,50 @@
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AMCLReportingWizard(models.TransientModel):
+    _name = 'amcl.reporting.wizard'
+
+    sale_order_id = fields.Many2one(comodel_name='sale.order', string='Sale Order')
+    sale_order_state = fields.Selection(related='sale_order_id.state')
+
+    type_report = fields.Selection([('normal_confession', 'اقرار عادي'),
+                                    ('form_receipt', 'نموذج استلام'),
+                                    ('form_no_4_commitment_of_individuals', 'نموذج رقم 4 - تعهد الأفراد'),
+                                    ('user_acknowledgment_form', 'نموذج إقرار المستخدم')],
+                                   string='Chose Report Type', default='normal_confession')
+
+    catch_receipt = fields.Boolean(string='Catch Receipt', default=False)
+
+    def button_print_report_ordinary_confession_pdf(self):
+        self.ensure_one()
+        data = {
+            'id': self.id,
+            'model': 'amcl.reporting.wizard',
+        }
+        report_type = 'qweb-html'
+        report_name = ''
+
+        if self.catch_receipt:
+            report_name += 'amcl_al_emlak.report_catch_receipt_report_view'
+
+        elif str(self.type_report) == "form_receipt":
+            report_name += 'amcl_al_emlak.report_sale_order_receipt_form_view'
+
+        elif self.type_report == 'normal_confession':
+            report_name += 'amcl_al_emlak.report_sale_order_ordinary_confession_view'
+
+        elif self.type_report == 'form_no_4_commitment_of_individuals':
+            report_name += 'amcl_al_emlak.form_nb_4_commitment_of_individuals'
+
+        elif self.type_report == 'user_acknowledgment_form':
+            report_name += 'amcl_al_emlak.report_user_acknowledgment_form_view'
+        else:
+            raise ValidationError('الرجاء اختيار نوع التقرير المطلوب')
+
+        return self.env['ir.actions.report'].search(
+            [('report_name', '=', report_name),
+             ('report_type', '=', report_type)], limit=1).report_action(self, data=data, config=False)
+
