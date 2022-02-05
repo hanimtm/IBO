264eceada625b0ca8848faf01ea2de392d66f0c6
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-02-05 19:59:18 +0200

    convert amount to letter

--- amcl_al_emlak/models/amcl_payment_inherit.py
+++ amcl_al_emlak/models/amcl_payment_inherit.py
@@ -1,6 +1,14 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from openerp.tools.amount_to_text_en import amount_to_text
 
 
 class AMCLPaymentInherit(models.Model):
     _inherit = 'account.payment'
+
+    amount_in_word = fields.Char(compute='compute_amount_to_letter')
+
+    @api.depends('amount')
+    def compute_amount_to_letter(self):
+        for payment in self:
+            payment.amount_in_word = amount_to_text(amount)
 
