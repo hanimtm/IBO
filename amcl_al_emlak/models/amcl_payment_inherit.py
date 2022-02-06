df5a0c951adbdc7a268b02c427cd07673cea01a1
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-02-06 11:12:08 +0200

    edit

--- amcl_al_emlak/models/amcl_payment_inherit.py
+++ amcl_al_emlak/models/amcl_payment_inherit.py
@@ -1,14 +1,16 @@
 from odoo import fields, models, api
-# from openerp.tools.amount_to_text_en import amount_to_text
+from num2words import num2words
+
+# pip install num2words
 
 
 class AMCLPaymentInherit(models.Model):
     _inherit = 'account.payment'
 
-    amount_in_word = fields.Char()
-    # compute = 'compute_amount_to_letter'
-    # @api.depends('amount')
-    # def compute_amount_to_letter(self):
-    #     for payment in self:
-    #         payment.amount_in_word = payment.currency_id.amount_to_text(amount)
+    amount_in_word = fields.Char(compute='compute_amount_to_letter')
+
+    @api.depends('amount')
+    def compute_amount_to_letter(self):
+        for payment in self:
+            payment.amount_in_word = num2words(payment.amount)
 
